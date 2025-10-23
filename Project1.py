2e0f420f46962a2a41765362c7aa25d47aff2a7b
Juan <jagusti1@bruinmail.slcc.edu>
2025-10-22 18:49:40 -0600

    Project 1 in Python

---
+++ Project1.py
@@ -0,0 +1,12 @@
+side = 8
+# addition - +
+# Substraction - -
+# multiplication - *
+# division - /
+area = side * side
+
+fish = "Tuna"
+
+Animal = fish
+
+print("Side = " + str(side) +"area =  "+ str(area) +"fish: "+ fish +"Animal" + Animal)
